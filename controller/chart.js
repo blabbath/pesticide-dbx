0fcee3590aa78476a0659609bfb0db6cc1f99c40
blabbath <franz.poellinger@gmx.de>
2020-12-16 14:05:55 +0100

    HRI all actGrps available

--- controller/chart.js
+++ controller/chart.js
@@ -148,7 +148,7 @@ const controller = {
                 actGrps: [
                     'Herbizide',
                     'Insektizide',
-                    /* 'Andere', */
+                    'Andere',
                     'Wachstumsregler',
                     'Fungizide',
                 ].sort(),
