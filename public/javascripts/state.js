4ffe96d291baffa043b077dfabc12e55069f6558
blabbath <franz.poellinger@gmx.de>
2021-01-12 09:43:38 +0100

    Update state sub_grps - overflow: auto

--- public/javascripts/state.js
+++ public/javascripts/state.js
@@ -6,12 +6,14 @@ import '../scss/state.scss';
 import '@clr/icons/shapes/chart-shapes';
 import { closeAlert } from './index/alert';
 
+[essentialAssets];
+
 closeAlert.timeOut();
 closeAlert.click();
 
-//let tableCell = document.querySelectorAll('.div-sub-grps');
+let tableCell = document.querySelectorAll('.div-sub-grps');
 
-/* for (const e of tableCell) {
+for (const e of tableCell) {
     let children = Array.prototype.slice.call(e.children); // turn HTMLCollection into Array
     let childrenShow = children.filter(child => child.classList.contains('sub-table-show'));
     if (
@@ -33,45 +35,4 @@ closeAlert.click();
             return false;
         });
     }
-} */
-
-const ShowAllSubs = class ShowAllSubs {
-    constructor() {
-        this.tableCell = document.querySelectorAll('.div-sub-grps');
-        this.children = undefined;
-        this.childrenShow = undefined;
-    }
-
-    showHide() {
-        let t = this;
-        for (const e of t.tableCell) {
-            t.children = Array.prototype.slice.call(e.children); // turn HTMLCollection into Array
-            t.childrenShow = t.children.filter(child => child.classList.contains('sub-table-show'));
-            if (
-                t.childrenShow.some(child => child.classList.contains('sub-table-show')) &&
-                t.childrenShow.filter(child => child.classList.contains('sub-table-show')).length > 5
-            ) {
-                e.addEventListener('mouseenter', () => {
-                    t.children
-                        .filter(child => child.classList.contains('sub-table'))
-                        .forEach(c => (c.style.display = 'none')); //hide the 5 present sub_grps
-                    t.childrenShow.forEach(c => (c.style.display = 'block')); //show all sub_grps
-                    //return false;
-                });
-                e.addEventListener('mouseleave', () => {
-                    t.children
-                        .filter(child => child.classList.contains('sub-table'))
-                        .forEach(c => (c.style.display = 'block')); //show 5 sub_grps
-                    t.childrenShow.forEach(c => (c.style.display = 'none')); //hide all sub_grps
-                    //return false;
-                });
-            }
-        }
-    }
 }
-
-let showAllSubs = new ShowAllSubs();
-console.log(showAllSubs)
-showAllSubs.showHide
-
-[essentialAssets];
