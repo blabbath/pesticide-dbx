1335b758fb18ffb7b81f034e0105650a516a9cf3
blabbath <franz.poellinger@gmx.de>
2021-01-22 11:28:54 +0100

    Removed admin backend data routes

--- routes/admin.js
+++ routes/admin.js
@@ -11,20 +11,6 @@ class projectRouter {
     setRoutes() {
         //Admin
         router.get('/', middleware.isAdmin, controller.index(this.viewPath));
-
-        router.get(
-            '/chart',
-            [middleware.isAdmin, middleware.getSavedState],
-            controller.chart(this.viewPath)
-        );
-
-        router.post('/receive_data', middleware.isAdmin, controller.receiveData);
-
-        router.get('/risk_ind', middleware.isAdmin, controller.sendRiskInd);
-
-        router.get('/subgrps_admin', middleware.isAdmin, controller.sendSubgrps);
-
-        router.get('/visData_admin', middleware.isAdmin, controller.sendData);
     }
 
     getRoutes() {
